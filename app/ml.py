1bef954382f5d8119384683ac912a9a628d68c04
TomasFouyet <80767637+TomasFouyet@users.noreply.github.com>
2024-11-26 21:43:19 -0300

    Modificacion de ML

--- app/ml.py
+++ app/ml.py
@@ -1,7 +1,9 @@
-import spacy
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 class RecipeModel:
+    
+    
+    
     def __init__(self):
         # Cargar modelo y tokenizador preentrenado
         self.tokenizer = AutoTokenizer.from_pretrained("mbien/recipenlg")
@@ -12,22 +14,37 @@ class RecipeModel:
 
         # Ajustar dimensiones del modelo para incluir el nuevo token
         self.model.resize_token_embeddings(len(self.tokenizer), mean_resizing=False)
+    
 
-    def generate_recipe(self, ingredients: str, max_length: int = 150) -> str:
+    def generate_recipe(self, ingredients: list, max_length: int = 150) -> str:
         # Tokenizar entrada con padding y truncado
-        inputs = self.tokenizer.encode(ingredients, return_tensors="pt")
-
+        print(ingredients)
+        ingredients_formatted = "<INPUT_START> " + " <NEXT_INPUT> ".join(ingredients) + " <INPUT_END>"
+        print(ingredients_formatted)
+        inputs = self.tokenizer.encode(ingredients_formatted, return_tensors="pt")
+        print(inputs)
         # Generar receta
         outputs = self.model.generate(
             inputs,
             num_return_sequences=1,
-            max_length=150,
+            max_length=300,
             pad_token_id=self.tokenizer.pad_token_id,
             no_repeat_ngram_size=3,
-            num_beams=10,
-            temperature=0.3,
-            do_sample=False
+            num_beams=6,
+            temperature=1,
+            do_sample=True
         )
-
-        recipe_text = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        recipe_text = self.tokenizer.decode(outputs[0])
+           # Truncar después del primer <TITLE_END>
+        print(recipe_text)
+        if "<TITLE_END>" in recipe_text:
+            recipe_text = recipe_text.split("<TITLE_END>")[0] + "<TITLE_END>"
+        # Remover todos los tokens
+        lista_tokens = ["<RECIPE_START>", "<INPUT_START>", "<NEXT_INPUT>", "<INPUT_END>", "<TITLE_START>", "<TITLE_END>", "<INGR_START>", "<NEXT_INGR>", "<INGR_END>", "<INSTR_START>", "<NEXT_INSTR>", "<INSTR_END>", "<RECIPE_END>", "\t", "\n"]
+        for token in lista_tokens:
+            recipe_text = recipe_text.replace(token, "")
+        # Remover espacios duplicados
+        
+        
+        
         return recipe_text.strip()
