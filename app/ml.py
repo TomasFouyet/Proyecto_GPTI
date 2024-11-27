f77cd826989178b98d99642a59a5a9b35da442b0
TomasFouyet <80767637+TomasFouyet@users.noreply.github.com>
2024-11-26 21:45:44 -0300

    Eliminar prints

--- app/ml.py
+++ app/ml.py
@@ -18,11 +18,8 @@ class RecipeModel:
 
     def generate_recipe(self, ingredients: list, max_length: int = 150) -> str:
         # Tokenizar entrada con padding y truncado
-        print(ingredients)
         ingredients_formatted = "<INPUT_START> " + " <NEXT_INPUT> ".join(ingredients) + " <INPUT_END>"
-        print(ingredients_formatted)
         inputs = self.tokenizer.encode(ingredients_formatted, return_tensors="pt")
-        print(inputs)
         # Generar receta
         outputs = self.model.generate(
             inputs,
@@ -36,15 +33,10 @@ class RecipeModel:
         )
         recipe_text = self.tokenizer.decode(outputs[0])
            # Truncar después del primer <TITLE_END>
-        print(recipe_text)
         if "<TITLE_END>" in recipe_text:
             recipe_text = recipe_text.split("<TITLE_END>")[0] + "<TITLE_END>"
         # Remover todos los tokens
         lista_tokens = ["<RECIPE_START>", "<INPUT_START>", "<NEXT_INPUT>", "<INPUT_END>", "<TITLE_START>", "<TITLE_END>", "<INGR_START>", "<NEXT_INGR>", "<INGR_END>", "<INSTR_START>", "<NEXT_INSTR>", "<INSTR_END>", "<RECIPE_END>", "\t", "\n"]
         for token in lista_tokens:
             recipe_text = recipe_text.replace(token, "")
-        # Remover espacios duplicados
-        
-        
-        
         return recipe_text.strip()
